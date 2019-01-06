81d2221a495f85297a7d652a4bb024f8986d12e1
Thailan <thailan-higor@hotmail.com>
2019-01-06 20:28:08 -0200

    Criando Página Main

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,12 +1,44 @@
 import React from 'react';
-import { View, StatusBar } from 'react-native';
+import { View, StatusBar, Text, TextInput, TouchableOpacity, SafeAreaView } from 'react-native';
 
 import styles from './styles';
 
 const Main = () => (
-  <View style={styles.container}>
+  <SafeAreaView style={styles.container}>
     <StatusBar barStyle={'light-content'} />
-  </View>
+
+    <View style={styles.content}>
+      <Text style={styles.title}>GitMark</Text>
+      <Text style={styles.description}>
+        Comece adicionando alguns repositórios aos seus favoritos
+      </Text>
+
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          autoCaptalize="none"
+          autoCorrect={false}
+          placeholder="usuário/repositório"
+          underlineColorAndroid="transparent"
+        />
+
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() =>{}}
+          activeOpacity={0.6}
+        >
+          <Text style={styles.buttonText}>Adicionar aos favoritos </Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+
+    <View style={styles.footer}>
+      <TouchableOpacity  onPress={() => {}}>
+        <Text style={styles.footerLink}> Meus Favoritos (3) </Text>
+      </TouchableOpacity>
+    </View>
+
+  </SafeAreaView>
 );
 
 Main.navigationOptions = {
--- src/pages/main/styles.js
+++ src/pages/main/styles.js
@@ -1,11 +1,72 @@
 import { StyleSheet } from 'react-native';
-import { colors } from 'styles';
+import { colors, metrics } from 'styles';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: colors.primary,
+    alignItems: 'center',
   },
+
+  content: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingHorizontal: metrics.basePadding,
+  },
+
+  title: {
+    fontSize: 30,
+    color: colors.white,
+    fontWeight: 'bold',
+  },
+
+  description: {
+    color: colors.white,
+    fontSize: 12,
+    lineHeight: 20,
+    marginTop: metrics.baseMargin / 2,
+    textAlign: 'center',
+  },
+
+  form: {
+    marginTop: metrics.baseMargin * 2,
+    alignSelf: 'stretch',
+  },
+
+  input: {
+    height: 40,
+    backgroundColor: colors.white,
+    borderRadius: metrics.baseRadius,
+    paddingHorizontal: metrics.basePadding,
+  },
+
+  button: {
+    height: 40,
+    backgroundColor: colors.secundary,
+    borderRadius: metrics.baseRadius,
+    paddingHorizontal: metrics.basePadding,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: metrics.baseMargin,
+  },
+
+  buttonText: {
+    color: colors.darkTransparent,
+    fontWeight: 'bold',
+    fontSize: 12,
+  },
+
+  footer: {
+    paddingBottom: metrics.basePadding,
+  },
+
+  footerLink: {
+    color: colors.white,
+    fontWeight: 'bold',
+    fontSize: 12,
+  },
+
 });
 
 export default styles;
