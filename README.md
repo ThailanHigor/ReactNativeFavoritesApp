e654499a59ef52b221cf3ac682eb9b1ad09c33e8
Thailan <thailan-higor@hotmail.com>
2019-01-13 20:06:25 -0200

    Configurando Plugin Saga no reactotron
    comando executado:
    yarn add reactotron-redux-saga

--- README.md
+++ README.md
@@ -6,4 +6,4 @@ yarn add prop-types
 yarn add reactotron-redux
 yarn add color
 yarn add axios redux-saga
-
+yarn add reactotron-redux-saga
--- src/config/ReactotronConfig.js
+++ src/config/ReactotronConfig.js
@@ -1,11 +1,13 @@
 import Reactotron from 'reactotron-react-native';
 import { reactotronRedux } from 'reactotron-redux';
+import sagaPlugin from 'reactotron-redux-saga';
 
 if (__DEV__) {
   const tron = Reactotron
     .configure({ host: '192.168.29.67' })
     .useReactNative()
     .use(reactotronRedux())
+    .use(sagaPlugin())
     .connect();
 
   tron.clear();
